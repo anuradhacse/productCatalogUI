64fe89c00b0cdc69a6f30dd28625e91f35420d03
ajayalath <ajayalath@wiley.com>
2021-09-13 11:36:33 +0530

    created intial project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React from 'react';
+
+
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+
+import productList from './productList';
+import priceCalculator from './calculator';
+
+function App() {
+
+  return (
+    <div className="App">
+    <Router>
+      <Route path='/' exact component={productList} />
+      <Route path='/price-calculator' component={priceCalculator} />
+    </Router>
+      
+  </div>
+  );
+}
+
+export default App;
